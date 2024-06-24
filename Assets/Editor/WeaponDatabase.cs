f9a5310a1f1ad3bd482e68659d9be9b248e32c79
Lin <100497279+Collinb190@users.noreply.github.com>
2024-06-24 11:48:03 -0500

    editor script bones

---
+++ Assets/Editor/WeaponDatabase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WeaponDatabase : ItemDatabase<Weapon>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void ShowWindow()
+    {
+        GetWindow<WeaponDatabase>("Weapon Database");
+    }
+
+    protected override void DrawItemList()
+    {
+        Debug.Log("Drawing the item list");
+    }
+
+    protected override void DrawPropertiesSection()
+    {
+        Debug.Log("Drawing the properties section");
+    }
+
+    protected override void ExportItemsToCSV()
+    {
+        // Implement exporting weapons to CSV
+        Debug.Log("Exporting weapons to CSV...");
+    }
+
+    protected override void ImportItemsFromCSV()
+    {
+        // Implement importing weapons from CSV
+        Debug.Log("Importing weapons from CSV...");
+    }
+}
